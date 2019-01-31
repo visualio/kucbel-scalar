6b7c70cbb05e6e7af5cab02822b3cfabb3c78a2e
David Kucbel <davidk@xcroco.com>
2019-01-31 12:57:39 +0100

    Callback filter

---
+++ src/Filter/CallFilter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Kucbel\Scalar\Filter;
+
+class CallFilter extends Filter
+{
+	/**
+	 * @var callable
+	 */
+	private $callback;
+
+	/**
+	 * @var callable
+	 */
+	private $condition;
+
+	/**
+	 * CallbackFilter constructor.
+	 *
+	 * @param callable $callback
+	 * @param callable ...$conditions
+	 */
+	function __construct( callable $callback, callable ...$conditions )
+	{
+		$this->callback = $callback;
+		$this->condition = self::merge( ...$conditions );
+	}
+
+	/**
+	 * @param mixed $value
+	 * @return mixed
+	 */
+	function clear( $value )
+	{
+		if( call_user_func( $this->condition, $value )) {
+			$value = call_user_func( $this->callback, $value );
+		}
+
+		return $value;
+	}
+
+	/**
+	 * @param callable ...$conditions
+	 * @return callable
+	 */
+	static function merge( callable ...$conditions ) : callable
+	{
+		if( isset( $conditions[1] )) {
+			return function( $value ) use( $conditions ) {
+				foreach( $conditions as $condition ) {
+					if( call_user_func( $condition, $value )) {
+						return true;
+					}
+				}
+
+				return false;
+			};
+		} elseif( isset( $conditions[0] )) {
+			return $conditions[0];
+		} else {
+			return function() {
+				return true;
+			};
+		}
+	}
+}
